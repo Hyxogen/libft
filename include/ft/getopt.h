8156aed50b71f572224a5ad554b36f4eed491c64
Hyxogen <8938732+Hyxogen@users.noreply.github.com>
2024-02-28 11:40:10 +0100

    add ft_getopt declarations to header

--- include/ft/getopt.h
+++ include/ft/getopt.h
@@ -3,6 +3,9 @@
 
 #include <getopt.h>
 
+extern int ft_optind, ft_opterr, ft_optopt;
+extern char *ft_optarg;
+
 int ft_getopt(int argc, char **argv, const char *optstring);
 int ft_getopt_long(int argc, char **argv, const char *optstring,
 		   const struct option *longopts, int *longindex);
